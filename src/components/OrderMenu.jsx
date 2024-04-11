ba3bb2bef245e3de762cd485f99c4d0196210229
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-04-12 01:19:33 +0530

    fixed images path

--- src/components/OrderMenu.jsx
+++ src/components/OrderMenu.jsx
@@ -8,16 +8,16 @@ const OrderMenu = ({ cafe }) => {
       return (
         <div className='font-Poppins p-12'>
         <CafeCard name={"Maggi Hotspot Order Menu"} color={"text-primary-red"} gradient={"from-red-200 to-red-300"}>  
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
-          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./src/assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
+          <FoodCard name={"Maggi"} price={"45"}  buttonColor={"bg-primary-red"} image={"./assets/maggie.jpeg"}></FoodCard>
         </CafeCard>
       </div>
       )
@@ -26,16 +26,16 @@ const OrderMenu = ({ cafe }) => {
       return (
       <div className='font-Poppins p-12'>
       <CafeCard name={"Southern Stories Order Menu"} color={"text-primary-beige"} gradient={"from-orange-100 to-orange-50"}>  
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
-      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./src/assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
+      <FoodCard name={"Dosa"} price={"80"} buttonColor={"bg-primary-beige"} image={"./assets/dosa.jpeg"}></FoodCard>
       </CafeCard>
       </div>)
     }
@@ -44,16 +44,16 @@ const OrderMenu = ({ cafe }) => {
       return (
       <div className='font-Poppins p-12'>
       <CafeCard name={"Kathi & COP Order Menu"} color={"text-primary-orange"} gradient={"from-orange-300 to-orange-200"}>  
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
-      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./src/assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
+      <FoodCard name={"Chole Bhature"} price={"80"} buttonColor={"bg-primary-orange"} image={"./assets/chole.png"}></FoodCard>
       </CafeCard>
       </div>)
     }
@@ -62,16 +62,16 @@ const OrderMenu = ({ cafe }) => {
       return(
       <div className='font-Poppins p-12'>  
       <CafeCard name={"Quench Order Menu"} color={"text-primary-green"} gradient={"from-green-300 to-green-200"}>  
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
-      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./src/assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
+      <FoodCard name={"Peri Peri Fries"} price={"60"} buttonColor={"bg-primary-green"} image={"./assets/fries.png"}></FoodCard>
       </CafeCard>
       </div>
       )
