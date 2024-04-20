bb7540807803c1edfe317466f45169a3e9c7d78b
Divyansh Vishwakarma <divyanshvishwakarma4@gmail.com>
2024-04-21 01:02:23 +0530

    border removed

--- src/components/Example.jsx
+++ src/components/Example.jsx
@@ -97,7 +97,7 @@ export default function Example() {
                                   />
                                 </div>
 
-                                <div className="ml-4flex flex-1 flex-col border border-gray-300 mr-2 rounded-md p-2">
+                                <div className="ml-4flex flex-1 flex-col mr-2 rounded-md p-2">
                                   <div>
                                     <div className="flex justify-between text-base font-medium text-gray-900 ">
                                       <h3>
